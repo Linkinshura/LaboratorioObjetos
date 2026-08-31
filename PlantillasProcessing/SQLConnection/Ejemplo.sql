a2d6f5e677f148a2f0ad9d0ecaa0e1275377b7b4
NeroShura <ian.linkinshura@gmail.com>
2026-08-31 20:54:53 -0300

    Create Ejemplo.sql

---
+++ PlantillasProcessing/SQLConnection/Ejemplo.sql
@@ -0,0 +1,10 @@
+CREATE TABLE alumnos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100),
+    edad INT
+);
+
+INSERT INTO alumnos (nombre, edad) VALUES
+('Ana', 20),
+('Juan', 22),
+('Sofía', 19);
